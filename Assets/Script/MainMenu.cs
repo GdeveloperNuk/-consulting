ba60fdafbd3d15fd76c48447c93c9f0f085fc626
GdeveloperNuk <88584189+GdeveloperNuk@users.noreply.github.com>
2022-01-15 20:05:07 +0900

    new

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -1,42 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickNewGame()
     {
-        
-    }
-
-    public void OnClickNewGame() 
-    {
-        Debug.Log("새 게임");
-    }
-
-    public void OnClickLoad()
-    {
-        Debug.Log("불러오기");
-    }
-
-    public void OnClickOption()
-    {
-        Debug.Log("옵션");
-    }
-
-    public void OnClickQuit()
-    {
-#if UNITY_EDITOR
-       UnityEditor.EditorApplication.isPlaying = false;
-#else
-        Application.Quit();
-#endif
+        SceneManager.LoadScene("main_scene");
     }
 }
